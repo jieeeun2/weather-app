dfc358e7d6a6c182dfbca478f134ac1e4895804f
jieeeun2 <jieun2.dev@gmail.com>
2024-07-05 18:27:11 +0900

    feat: 정적,동적 메타데이터 추가 (+)
    동적메타데이터 설정하기 위한 방법에서 `params`는 경로파라미터로 오는 값이고 `searchParams`는 쿼리스트링으로 오는 값

--- src/app/[location]/page.tsx
+++ src/app/[location]/page.tsx
@@ -4,14 +4,25 @@ import { getForecast } from '@/utils/getForecast'
 type Props = {
   params: {
     location: string
+  },
+  searchParams: {
+    name: string
   }
 }
-export default async function Detail({ params }: Props) {
+
+export function generateMetadata({ searchParams }: Props) {
+  return {
+    title: `날씨 앱 - ${searchParams.name}`,
+    description: `${searchParams.name} 날씨를 알려드립니다`
+  }
+}
+
+export default async function Detail({ params, searchParams }: Props) {
   const res = await getForecast(params.location)
 
   return (
   <>
-    <h1>{params.location}의 3일 예보</h1>
+    <h1>{searchParams.name}의 3일 예보</h1>
     <ul>
       {res.forecast.forecastday.map((day) => (
         <li key={day.date}>
