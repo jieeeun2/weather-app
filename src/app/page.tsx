4090df38641f50fa315e86ae082057016acb9ed6
jieeeun2 <jieun2.dev@gmail.com>
2024-07-05 14:43:14 +0900

    design: 스타일링 (+)
    next에서 전역스타일링은 app 바로아래에 global.css
    글로벌스타일링은 적용할 page파일과 같은 깊이에서 style.module.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Link from 'next/link'
+import style from './style.module.css'
 
 export default function Home() {
   return (
   <>
     <h1>날씨 앱</h1>
-    <ul>
+    <ul className={style.list}>
       <li><Link href='/seoul'>서울</Link></li>
       <li><Link href='/newyork'>뉴욕</Link></li>
       <li><Link href='/london'>런던</Link></li>
     </ul>
+    <button>테스트</button>
   </>
   )
 }
