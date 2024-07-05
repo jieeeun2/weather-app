8bccf841e23c164f8fa647421a3636d627cb21fc
jieeeun2 <jieun2.dev@gmail.com>
2024-07-05 15:51:08 +0900

    feat: api호출 src/utils폴더 내부로 분리 및 결과값 화면에 표출 (+)
    getCurrentWeather: 현재날씨. app/page.tsx에서 호출
    getForecast: 3일예보. app/[location]/page.tsx에서 호출

--- src/app/[location]/page.tsx
+++ src/app/[location]/page.tsx
@@ -1,14 +1,24 @@
 import HomeButton from '@/components/HomeButton'
+import { getForecast } from '@/utils/getForecast'
 
 type Props = {
   params: {
     location: string
   }
 }
-export default function Detail({ params }: Props) {
+export default async function Detail({ params }: Props) {
+  const res = await getForecast(params.location)
+
   return (
   <>
     <h1>{params.location}의 3일 예보</h1>
+    <ul>
+      {res.forecast.forecastday.map((day) => (
+        <li key={day.date}>
+          {day.date} / {day.day.avgtemp_c}
+        </li>
+      ))}
+    </ul>
     <HomeButton />
   </>
   )
