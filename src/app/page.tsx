043c5dbb20b051bc885e3cb4a4f3f6f0cf5bb3d1
jieeeun2 <jieun2.dev@gmail.com>
2024-07-05 15:11:56 +0900

    feat: WeatherAPI.com 이용한 날씨데이터 조회

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
 import Link from 'next/link'
 import style from './style.module.css'
 
-export default function Home() {
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY
+//이 서버컴포넌트 실행환경이 node이기에 process.env 사용가능
+
+const getCurrentWeather = async () => {
+  const res = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Seoul&aqi=no`
+  )
+
+  return res.json()
+}
+
+export default async function Home() {
+  const res = await getCurrentWeather()
+  console.log(res)
+
   return (
   <>
     <h1>날씨 앱</h1>
